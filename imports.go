15e60216a3435f26c6a13a1b507399c0b1f07f84
Петр иванов <petara94@mail.ru>
2025-05-02 01:50:32 +0300

    junk commit all

---
+++ imports.go
@@ -0,0 +1,7 @@
+package main
+
+import "google.golang.org/protobuf/compiler/protogen"
+
+var (
+	fiberImport = protogen.GoImportPath("github.com/gofiber/fiber/v2")
+)
---
+++ options.go
@@ -0,0 +1,12 @@
+package main
+
+import "flag"
+
+var (
+	flags                         = flag.NewFlagSet("protoc-gen-go-fiber", flag.ExitOnError)
+	flagErrorHandlersPackage      = flags.String("error_handlers_package", "github.com/petara94/protoc-gen-go-fiber/utils", "package with error handlers funcs")
+	flagGrpcErrorHandleFunc       = flags.String("grpc_error_handle_func", "HandleGRPCStatusError", "func name for handle grpc error")
+	flagUnmarshalErrorHandleFunc  = flags.String("unmarshal_error_handle_func", "HandleUnmarshalError", "func name for handle unmarshal error")
+	flagValidationErrorHandleFunc = flags.String("validation_error_handle_func", "HandleValidationError", "func name for handle validation error")
+	flagDisableGrpcInterceptor    = flags.Bool("disable_grpc_interceptor", false, "disable grpc interceptor")
+)
