33b25da139e9c2bec2315265e9f723eccf2eef06
Петр Иванов <ivanoppeter@anykey.pro>
2025-05-10 05:26:55 +0300

    refactoring final

--- imports.go
+++ imports.go
@@ -5,8 +5,8 @@ import "google.golang.org/protobuf/compiler/protogen"
 var (
 	fiberImport         = protogen.GoImportPath("github.com/gofiber/fiber/v2")
 	contextImport       = protogen.GoImportPath("context")
-	jsonUnmarshalImport = protogen.GoImportPath("encoding/json")
 	grpcMetadataImport  = protogen.GoImportPath("google.golang.org/grpc/metadata")
 	grpcImport          = protogen.GoImportPath("google.golang.org/grpc")
-	errorHandlersImport protogen.GoImportPath
+	errorHandlersImport = protogen.GoImportPath(defaultFlagErrorHandlersPackage)
+	jsonUnmarshalImport = protogen.GoImportPath(defaultJsonUnmarshalPackage)
 )
--- options.go
+++ options.go
@@ -5,10 +5,15 @@ import (
 	"google.golang.org/protobuf/compiler/protogen"
 )
 
+const (
+	defaultFlagErrorHandlersPackage = "github.com/petara94/protoc-gen-go-fiber/utils"
+	defaultJsonUnmarshalPackage     = "encoding/json"
+)
+
 var (
 	flags                         = flag.NewFlagSet("protoc-gen-go-fiber", flag.ExitOnError)
-	flagErrorHandlersPackage      = flags.String("error_handlers_package", "github.com/petara94/protoc-gen-go-fiber/utils", "package with error handlers funcs")
-	flagJsonUnmarshalPackage      = flags.String("json_unmarshal_package", "encoding/json", "package with json unmarshalers")
+	flagErrorHandlersPackage      = flags.String("error_handlers_package", defaultFlagErrorHandlersPackage, "package with error handlers funcs")
+	flagJsonUnmarshalPackage      = flags.String("json_unmarshal_package", defaultJsonUnmarshalPackage, "package with json unmarshalers")
 	flagGrpcErrorHandleFunc       = flags.String("grpc_error_handle_func", "HandleGRPCStatusError", "func name for handle grpc error")
 	flagUnmarshalErrorHandleFunc  = flags.String("unmarshal_error_handle_func", "HandleUnmarshalError", "func name for handle unmarshal error")
 	flagValidationErrorHandleFunc = flags.String("validation_error_handle_func", "HandleValidationError", "func name for handle validation error")
